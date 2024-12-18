3b4cdffe86115787e51e85d76cfc38673e410549
Thoai Xuan <thoailata1000@gmail.com>
2024-12-18 22:12:14 +0700

    update

---
+++ models/model.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Sequential, Linear, Sigmoid
+from torch_scatter import scatter_add#, scatter_sub  # no scatter sub in lastest PyG
+from torch.nn import Parameter
+import numpy as np
+from utils.masking import TriangularCausalMask, ProbMask
+from models.encoder import Encoder, EncoderLayer, ConvLayer, EncoderStack
+from models.decoder import Decoder, DecoderLayer
+from models.attn import FullAttention, ProbAttention, AttentionLayer
+from models.embed import DataEmbedding
+
+class GNN_Transfomer(nn.Module):
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len,edge_index, edge_attr, wind_mean, wind_std,
+                factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
+                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu', 
+                output_attention = False, distil=True, mix=True,
+                device=torch.device('cuda:0')):
+        super(GNN_Transfomer, self).__init__()
+        self.pred_len = out_len
+        self.attn = attn
+        self.output_attention = output_attention
+        self.device =device
+        #GNN
+        self.graph_gnn = GraphGNN(self.device, edge_index, edge_attr, wind_mean, wind_std)
+        # Encoding
+        self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
+        self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
+        # Attention
+        Attn = ProbAttention if attn=='prob' else FullAttention
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    dropout=dropout,
+                    activation=activation
+                ) for l in range(e_layers)
+            ],
+            [
+                ConvLayer(
+                    d_model
+                ) for l in range(e_layers-1)
+            ] if distil else None,
+            norm_layer=torch.nn.LayerNorm(d_model)
+        )
+        # Decoder
+        self.decoder = Decoder(
+            [
+                DecoderLayer(
+                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=mix),
+                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False), 
+                                d_model, n_heads, mix=False),
+                    d_model,
+                    d_ff,
+                    dropout=dropout,
+                    activation=activation,
+                )
+                for l in range(d_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(d_model)
+        )
+        # self.end_conv1 = nn.Conv1d(in_channels=label_len+out_len, out_channels=out_len, kernel_size=1, bias=True)
+        # self.end_conv2 = nn.Conv1d(in_channels=d_model, out_channels=c_out, kernel_size=1, bias=True)
+        self.projection = nn.Linear(d_model, c_out, bias=True)
+        
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+
+        xn = []
+        for k in range(x_enc.shape[1]):
+            xn_gnn = x_enc[:,k]
+            xn_gnn = xn_gnn.contiguous()
+            xn_gnn = self.graph_gnn(xn_gnn)
+            xn.append(xn_gnn)
+        xn = torch.stack(xn, dim=1).squeeze()
+        xn = xn.view(32,96,30)
+        x_enc = x_enc[:,:,:,0]
+        x_enc = torch.cat([xn, x_enc], dim=-1)
+
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+
+        yn = []
+        for k in range(x_dec.shape[1]):
+            xn_gnn = x_dec[:,k]
+            xn_gnn = xn_gnn.contiguous()
+            xn_gnn = self.graph_gnn(xn_gnn)
+            yn.append(xn_gnn)
+        yn = torch.stack(yn, dim=1).squeeze()
+        yn = yn.view(32,yn.shape[1],30)
+        x_dec = x_dec[:,:,:,0]
+        x_dec = torch.cat([yn, x_dec], dim=-1)
+
+        dec_out = self.dec_embedding(x_dec, x_mark_dec)
+        dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
+        dec_out = self.projection(dec_out)
+
+        if self.output_attention:
+            return dec_out[:,-self.pred_len:,:], attns
+        else:
+            return dec_out[:,-self.pred_len:,:] # [B, L, D]
+
+
+class GraphGNN(nn.Module):
+    def __init__(self, device, edge_index, edge_attr, wind_mean, wind_std):
+        super(GraphGNN, self).__init__()
+        self.device = device
+        self.edge_index = torch.LongTensor(edge_index).to(self.device)
+        self.edge_attr = torch.Tensor(np.float32(edge_attr))
+        self.edge_attr_norm = (self.edge_attr - self.edge_attr.mean(dim=0)) / self.edge_attr.std(dim=0)
+        self.w = Parameter(torch.rand([1]))
+        self.b = Parameter(torch.rand([1]))
+        self.wind_mean = torch.Tensor(np.float32(wind_mean)).to(self.device)
+        self.wind_std = torch.Tensor(np.float32(wind_std)).to(self.device)
+        e_h = 16
+        e_out = 30
+        in_dim = 3
+        n_out = 3
+        self.edge_mlp = Sequential(Linear(in_dim * 2 + 2 + 1, e_h),
+                                   Sigmoid(),
+                                   Linear(e_h, e_out),
+                                   Sigmoid(),
+                                   )
+        self.node_mlp = Sequential(Linear(e_out, n_out),
+                                   Sigmoid(),
+                                   )
+
+    def forward(self, x):
+        self.edge_index = self.edge_index.to(self.device)
+        self.edge_attr = self.edge_attr.to(self.device)
+        self.w = self.w.to(self.device)
+        self.b = self.b.to(self.device)
+
+        edge_src, edge_target = self.edge_index
+        node_src = x[:, edge_src]
+        node_target = x[:, edge_target]
+
+        src_wind = node_src[:,:,-2:] * self.wind_std[None,None,:] + self.wind_mean[None,None,:]
+        src_wind_speed = src_wind[:, :, 0]
+        src_wind_direc = src_wind[:,:,1]
+        self.edge_attr_ = self.edge_attr[None, :, :].repeat(node_src.size(0), 1, 1)
+        city_dist = self.edge_attr_[:,:,0]
+        city_direc = self.edge_attr_[:,:,1]
+
+        theta = torch.abs(city_direc - src_wind_direc)
+        edge_weight = F.relu(3 * src_wind_speed * torch.cos(theta) / city_dist)
+        edge_weight = edge_weight.to(self.device)
+        edge_attr_norm = self.edge_attr_norm[None, :, :].repeat(node_src.size(0), 1, 1).to(self.device)
+        out = torch.cat([node_src, node_target, edge_attr_norm, edge_weight[:,:,None]], dim=-1)
+
+        out = self.edge_mlp(out)
+        out_add = scatter_add(out, edge_target, dim=1, dim_size=x.size(1))
+        # out_sub = scatter_sub(out, edge_src, dim=1, dim_size=x.size(1))
+        out_sub = scatter_add(out.neg(), edge_src, dim=1, dim_size=x.size(1))  # For higher version of PyG.
+
+        out = out_add + out_sub
+        out = self.node_mlp(out)
+
+        return out
